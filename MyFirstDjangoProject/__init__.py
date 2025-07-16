e25714fd095404929eaf97fb1ba6fe7efd8957ac
Ram22an <118821139+Ram22an@users.noreply.github.com>
2025-07-17 00:31:58 +0530

    explaining files

---
+++ MyFirstDjangoProject/__init__.py
@@ -0,0 +1,7 @@
+# this is not used for most of the projects
+
+# package is a folder that contains collection of modules(.py files that define function, classes and variables)
+
+# __init__.py is a special file that indicates that the directory should be treated as a package
+
+# importing thing happens because of this file
--- MyFirstDjangoProject/wsgi.py
+++ MyFirstDjangoProject/wsgi.py
@@ -14,3 +14,10 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyFirstDjangoProject.settings')
 
 application = get_wsgi_application()
+
+
+# this is mainly concern with deployment server(like apache, nginx, etc.)
+#  wsgi is stand for web server gateway interface
+# how the server should interact with web application
+
+
