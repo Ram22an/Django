4a14ddc8d866780badc6b4f49cddf2f45fb78999
Ram22an <118821139+Ram22an@users.noreply.github.com>
2025-07-20 02:29:03 +0530

    Creating tables and then migrating them

--- employees/admin.py
+++ employees/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
+from .models import Employee
 
 # Register your models here.
 
 
 # **it is used to display our models in the django admin panel
 # **admin panel help us to do basic CRUD(Create, Read, Update, and Delete)
+
+# **after creating models and then migrating them
+# **To see the models in admin panel, we need to register them in the admin.py file
+
+# **admin.site.register(YourModelName)  Replace YourModelName with your actual model name
+
+admin.site.register(Employee)
+
 
