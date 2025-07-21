82f5f3336b9d8e192dc76ae27dcffe1e7bad1258
Ram22an <118821139+Ram22an@users.noreply.github.com>
2025-07-22 00:30:43 +0530

    Changing the comment

--- MyFirstDjangoProject/views.py
+++ MyFirstDjangoProject/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 def home(request):
     # return HttpResponse('')
-    # this simply return some html code
+    # **this simply return some html code
 
-    # this renders a whole template 
+    # **this renders a whole template 
     return render(request,'home.html')
 
