9e720a354f0f9b7d0aac317ab719946da3c15a30
Ram22an <118821139+Ram22an@users.noreply.github.com>
2025-07-22 01:00:40 +0530

    fetching data from database

--- MyFirstDjangoProject/views.py
+++ MyFirstDjangoProject/views.py
@@ -1,9 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from employees.models import Employee
 def home(request):
+    employees=Employee.objects.all()
+    # **every model have objects it is object oriented mapping
+
+    # **print(employees)
+    # **this will print the employees in the console
+
+    # **to send it in the html template
+    context={'employees':employees,}
+    # **this is a dictionary that will be passed to the template
+
     # return HttpResponse('')
     # **this simply return some html code
 
     # **this renders a whole template 
-    return render(request,'home.html')
+    return render(request,'home.html',context)
 
